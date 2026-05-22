f97dc7603ee766de73bab0880b13a13c0be8fdce
aminashadablari <amina.lari2008@gmail.com>
2026-05-22 23:37:18 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,18 +1,10 @@
-import { serialize } from 'cookie'
-
 export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end()
 
   const { password } = req.body
 
   if (password === process.env.APP_PASSWORD) {
-    res.setHeader('Set-Cookie', serialize('app-password', password, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
-    }))
+    res.setHeader('Set-Cookie', `app-password=${password}; HttpOnly; Secure; SameSite=Strict; Max-Age=${60 * 60 * 24 * 7}; Path=/`)
     return res.status(200).json({ ok: true })
   }
 
